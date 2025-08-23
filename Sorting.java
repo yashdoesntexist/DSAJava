da574cb363c90aa5b372417d4df514cf9a99206c
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-22 19:56:59 -0700

    bubble sort

--- Sorting.java
+++ Sorting.java
@@ -1,24 +1,26 @@
 package DSAJava;
 
 public class Sorting{
-    public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
+    public static void printArray(int arr[]){
+        for(int i =0; i< arr.length; i++){
             System.out.print(arr[i] + " ");
         }
     }
     public static void main(String args[]){
-        int[] arr = {7,8,3,1,2};
-        // bubble sort 
-        for(int i =0; i <arr.length-1; i++){
-            for(int j =0; j<arr.length-1-i; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]= arr[j+1];
-                    arr[j+1] = temp;
+        int[] array = {8,7,1,2,3};
+        // it aint much but its a start
+        // bubble sort with time complexity of O(n^2)
+        for(int i = 0; i<array.length-1; i++){
+            for(int j =0; j<array.length-i-1; j++){
+                if(array[j]>array[j+1]){ // j =8  and j+1 = 7
+                    int temp = array[j]; // temp = 8
+                    array[j] = array[j+1]; // j = 7
+                    array[j+1] = temp; // j+1 = 8
+
                 }
             }
         }
-        printArr(arr);
-        // O(n^2)
+
+        printArray(array);
     }
 }
