c44a1921f5e403fe2b7f955307dff588fc57c1ba
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-26 21:36:57 -0700

    more problem solved

--- practiceClass.java
+++ practiceClass.java
@@ -10,11 +10,30 @@ public class practiceClass {
         }
         System.out.println("Your average is " + sum/arr.length);
     }
+        
+    // 2.	Write a function to print the sum of all odd numbers from 1 to n.
+    public static void sumOfOddNum(int n){
+        int sum = 0;
+        if(n <= 0){
+            System.out.println("Enter an ending number greated than 1");
+        }else{
+            for(int i =0; i<=n; i++){
+                if(i%2 != 0){
+                    sum += i;
+                }
+            }
+        }
+        System.out.println("Sum of all odd numbers is " + sum);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 3 numbers to calculate the average");
-        int[] nums = { sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        // System.out.println("Enter 3 numbers to calculate the average");
+        // int[] nums = { sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        // sc.close();
+        // avgCalc(nums);
+        System.out.println("Please enter a number n to calculate sum of all odd num from 1 to n");
+        int n = sc.nextInt();
+        sumOfOddNum(n);
         sc.close();
-        avgCalc(nums);
     }
 }
