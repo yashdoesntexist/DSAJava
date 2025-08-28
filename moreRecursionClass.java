bdb4ac7d18fccf66b6e261b4b40c18e590902907
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-28 15:35:48 -0700

    moreRec

--- moreRecursionClass.java
+++ moreRecursionClass.java
@@ -1,138 +1,51 @@
 package DSAJava;
 
-import java.util.HashSet;
-
 public class moreRecursionClass {
-    public static void towerOfHanoi(int n, String src, String helper, String dest){
+    // Time complexity 2^n 
+    public static void towerOfHanoi(int n, String source, String helper, String destination){
         if(n==1){
-            System.out.println("transfer disk " + n + " from " + src + " to "+ dest);
+            System.out.println("Transfer disk " + n + " from " + source + " to " + destination );
             return;
         }
-        towerOfHanoi(n-1, src, dest, helper);
-        System.out.println("transfer disk " + n + " from " + src + " to "+ dest);
-        towerOfHanoi(n-1, helper, src, dest);
-
+        
+        towerOfHanoi(n-1, source, destination, helper);
+        System.out.println("Transfer disk " + n + " from " + source + " to " + destination);
+        towerOfHanoi(n-1, helper, source, destination);
     }
-    public static void reversePrint(String str, int idx){
-        if(idx == 0){
+    // O(n)
+    public static void printStringReverse(String str, int idx){
+        if(idx==0){
             System.out.println(str.charAt(idx));
             return;
         }
         System.out.println(str.charAt(idx));
-        reversePrint(str, idx-1);
+        printStringReverse(str, idx-1);
     }
-    public static int first = -1;
-    public static int last = -1;
-
-    public static void findOccurance(String str, int idx, char element){
+    // O(n)
+    public static int first =-1;
+    public static int last =-1;
+    public static void charCounter(String str, char element, int idx){
         if(idx == str.length()){
             System.out.println(first);
+            System.out.println(last);
             return;
         }
         char currChar = str.charAt(idx);
         if(currChar == element){
             if(first == -1){
                 first = idx;
-            
             }else{
                 last = idx;
             }
         }
 
-        findOccurance(str, idx+1, element);
-    }
-    public static boolean isSorted(int[] arr, int idx ){
-        if(idx == arr.length-1){
-            return true;
-        }
-        if(arr[idx] < arr[idx+1]){
-            return isSorted(arr, idx+1);
-        }else{
-            return false;
-        }
-
-    }
-    public static void moveAllX(String str, int idx, int count, String newString){
-        if(idx == str.length()){
-            for(int i =0; i<count; i++){
-                newString += 'x';
-            }
-            System.out.println(newString);
-            return;
-        }
-        char currChar = str.charAt(idx);
-        if(currChar == 'x'){
-            count ++;
-            moveAllX(str, idx+1, count, newString);
-        } else{
-            newString += currChar;
-            moveAllX(str, idx+1, count, newString);
-        }
-    }
-    public static boolean[] map = new boolean[26];
-
-    public static void removeDups(String str, int idx, String newString){
-        if(idx == str.length()){
-            System.out.println(newString);
-            return;
-        }
-        char currChar = str.charAt(idx);
-        if(map[currChar-'a']){
-            removeDups(str, idx+1, newString);
-        }else{
-            newString += currChar;
-            map[currChar-'a'] = true;
-            removeDups(str, idx+1, newString);
-        }
-    }
-    public static void printSub(String str, int idx, String newString){
-        if(idx == str.length()){
-            System.out.println(newString);
-            return;
-        }
-        char currChar = str.charAt(idx);
-
-        printSub(str, idx+1, newString+currChar);
-
-        printSub(str, idx+1, newString);
+        charCounter(str, element, idx+1);
     }
 
-        public static void subSeq(String str, int idx, String newString, HashSet<String> set){
-        if(idx == str.length()){
-            if(set.contains(newString)){
-                return;
-            }else{
-                System.out.println(newString);
-                set.add(newString);
-                return;
-            }
-        }
-        char currChar = str.charAt(idx);
-
-        subSeq(str, idx+1, newString+currChar, set);
-
-        subSeq(str, idx+1, newString, set);
-    }
 
     public static void main(String args[]){
-        HashSet<String> set = new HashSet<>();
-
-        // // Tower of Hanoi
-        // int n = 2;
-        // towerOfHanoi(n, "S tower", "h tower", "D tower");
-        // // O(2^n)
-
-        // String str = "abcasdaacdd";
-        // reversePrint(str, str.length()-1);
-
-        // findOccurance(str, 0, 'a');
-
-        // int arr[] = {1,3,5};
-        // System.out.println(isSorted(arr, 0));
-        String str = "aaa";
-        // moveAllX(str, 0, 0, "");
-        // removeDups(str, 0, "");
-        subSeq(str, 0, "", set);
-
+        // towerOfHanoi(3, "Source", "helper", "destination");
+        // printStringReverse("abcd", 3);
+        charCounter("abaacdaefaah", 'a', 0);
     }
 }
