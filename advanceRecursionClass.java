e79344ce26a425baf436d2481908c82d0adfe19f
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-29 16:16:25 -0700

    advance recursion

--- advanceRecursionClass.java
+++ advanceRecursionClass.java
@@ -1,5 +1,7 @@
 package DSAJava;
 
+import java.util.ArrayList;
+
 public class advanceRecursionClass {
     public static void printPerm(String str, String permutation){
         if(str.length() == 0){
@@ -12,9 +14,74 @@ public class advanceRecursionClass {
             printPerm(newString, permutation+currChar);
         }
     }
+
+    public static int countPaths(int i, int j, int n, int m){
+        if(i==n || j==m){
+            return 0;
+        }
+        if(i ==n-1 && j == m-1){
+            return 1;
+        }
+
+        // moving down 
+        int downPaths = countPaths(i+1, j, n, m);
+        // move right 
+        int rightPaths = countPaths(i, j+1, n, m);
+
+        return downPaths + rightPaths;
+    }
+
+    public static int placeTiles(int n, int m){
+        if(n==m){
+            return 2;
+        }
+        if(n<m){
+            return 1;
+        }
+
+        int verticalPlacement = placeTiles(n-m, m);
+        int horizontalPlacement = placeTiles(n-1, m);
+
+        return verticalPlacement+horizontalPlacement;
+    }
+
+    public static int invitePeople(int n){
+        if(n<=1){
+            return 1;
+        }
+        int singleInvite = invitePeople(n-1);
+        int pairInvite = (n-1) * invitePeople(n-2);
+        return singleInvite + pairInvite;
+    }
+    
+    public static void printSubset(ArrayList<Integer> subset){
+        for(int i =0; i<subset.size(); i++){
+            System.out.print(subset.get(i)+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubset(int n, ArrayList<Integer> subset){
+        if(n == 0){
+            System.out.println(subset);
+            return;
+        }
+        subset.add(n);
+        findSubset(n-1, subset);
+
+        subset.remove(subset.size()-1);
+        findSubset(n-1, subset);
+    }
+
+
     public static void main(String[] args) {
-        String str = "abc";
-        printPerm(str, "");
+        printPerm("abc", "");
+        System.out.println(countPaths(0, 0, 3, 3));
+        System.out.println(placeTiles(4, 2));
+        System.out.println(invitePeople(4));
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(4, subset);
+
         
     }
 }
