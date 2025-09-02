b161c52047eb13098a263129ed7a928085badb91
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-09-02 13:51:15 -0700

    mergeSort

--- mergeSortClass.java
+++ mergeSortClass.java
@@ -1,7 +1,67 @@
 package DSAJava;
 
+import java.util.Arrays;
+
 public class mergeSortClass {
     public static void main(String[] args) {
-        
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    // Merge Sort function
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Sort left half
+            mergeSort(arr, left, mid);
+
+            // Sort right half
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the two halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Merge function
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // Sizes of two subarrays
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        // Merge them back
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        // Copy remaining elements (if any)
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
     }
 }
