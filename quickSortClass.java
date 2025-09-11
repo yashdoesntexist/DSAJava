113154ea4fbe8c95b0603842fb49eb84cdf1919d
agentP <learnhacksandtricks@gmail.com>
2025-09-11 15:59:01 -0700

    finishing quick sort

--- quickSortClass.java
+++ quickSortClass.java
@@ -1,61 +1,39 @@
 package DSAJava;
 
-import java.util.Scanner;
-
 public class quickSortClass {
-
-    public static int partition(int[] arr, int low, int high) {
+    public static int partition(int[] arr, int low, int high ){
         int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+        int i = low-1;
+        for(int j =low; j<high; j++){
+            if(arr[j]< pivot){
                 i++;
-
-
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
         arr[high] = temp;
-
-        return i + 1;
+        return i;
     }
-
-    // QuickSort recursive method
     public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
+    if(low < high) {
+        int pivotIndex = partition(arr, low, high);
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex+1, high);
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // Take array input from user
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+    }
+    public static void main(String[] args){
+        int[] arr = {4, 3, 9, 5, 2, 8};
+        int n = arr.length;
+        quickSort(arr, 0, n-1);
+        for(int i = 0; i<n; i++){
+            System.out.print(arr[i]+ " ");
         }
+        System.out.println();
 
-
-        quickSort(arr, 0, n - 1);
-
-        System.out.println("Sorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
     }
 }
